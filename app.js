59d346fca0a20cd530f289e62774b06e32abf015
Stefan Koev <st.koev@outlook.com>
2022-10-10 14:28:14 +0300

    add button functionality, final commit

--- app.js
+++ app.js
@@ -20,4 +20,24 @@ const init = async () => {
   setupUi();
 };
 
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-container")) return;
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUi();
+});
+
 window.addEventListener("load", init);
