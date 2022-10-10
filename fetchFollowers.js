4387a91e18aca99c0b71773640e5ac5e3e4ea317
Stefan Koev <st.koev@outlook.com>
2022-10-10 11:29:07 +0300

    display followers

--- fetchFollowers.js
+++ fetchFollowers.js
@@ -1,5 +1,15 @@
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
 
-const fetchFollowers = async () => {}
+const fetchFollowers = async () => {
+  try {
+    const response = await fetch(url);
+    const data = response.json();
+    if (!response) throw new Error(`something went wrong: ${error}`);
 
-export default fetchFollowers
+    return data;
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
+};
+
+export default fetchFollowers;
